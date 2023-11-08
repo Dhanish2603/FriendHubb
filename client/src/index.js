2b1a5cba720da2978b92f11183212a0edb8d0b5c
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-11-08 21:50:58 +0530

    exam

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import {
+import {     
   persistStore,
   persistReducer,
   FLUSH,
